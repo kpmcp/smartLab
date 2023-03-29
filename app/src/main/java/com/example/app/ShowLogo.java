2eda51daa71dc7cfa4c6fab3eb69c90a1561bae5
competitor <competitor@kp11.ru>
2023-03-29 16:55:11 +0300

    AddedSplash

--- app/src/main/java/com/example/app/ShowLogo.java
+++ app/src/main/java/com/example/app/ShowLogo.java
@@ -3,19 +3,26 @@ package com.example.app;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.Objects;
 
-public class ShowLogo extends AppCompatActivity {
+public class ShowLogo extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.logo);
-        Objects.requireNonNull(getSupportActionBar()).hide();
+        int secondsDelayed = 1;
+        new Handler().postDelayed(new Runnable() {
+            public void run() {
+                startActivity(new Intent(ShowLogo.this, MainActivity.class));
+                finish();
+            }
+        }, secondsDelayed * 1000);
 
 //        View group1;
 
